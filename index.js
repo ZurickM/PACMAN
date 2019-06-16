f880f2d093f38c4cae4166f1948542bfd73a61aa
ZurickM <40615777+ZurickM@users.noreply.github.com>
2019-06-15 21:12:44 -0700

    firstcommit

---
+++ index.js
@@ -0,0 +1,6 @@
+function button(){
+    document.getElementById('img').style.visibility='visible';
+    }
+    function button2(){
+    document.getElementById('img').style.visibility='hidden';
+    }
